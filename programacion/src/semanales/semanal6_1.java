58f0d04babd30e7b554652fbc80e1d54fc0cb20a
Usuario <Usuario@LAPTOP-B5DJ2DJK>
2021-02-18 08:56:48 +0100

    semanal 6 terminado

--- programacion/src/semanales/semanal6_1.java
+++ programacion/src/semanales/semanal6_1.java
@@ -26,7 +26,9 @@ public class semanal6_1 {
 			int[] numerolr1;
 			String frase2;
 			char[] letrasf2;
-			int[] numerolr2;			
+			int[] numerolr2;
+			int repetidas=0;
+			int letrastotales=0;
 			
 			frase1=pedir(sct);
 			frase2=pedir(sct);
@@ -34,16 +36,53 @@ public class semanal6_1 {
 			letrasf1=frase1.toCharArray();
 			letrasf2=frase2.toCharArray();
 			
-			if(letrasf1==letrasf2) {
+			if(letrasf1.length==letrasf2.length) {
 				numerolr1= new int[letrasf1.length];
 				repetidos(letrasf1, numerolr1);
+				eliminarrepetidos(letrasf1, numerolr1);
 				numerolr2= new int[letrasf2.length];
 				repetidos(letrasf2, numerolr2);
+				eliminarrepetidos(letrasf2, numerolr2);
+				
+				for(int i=0; i<letrasf1.length; i++) {
+					for(int j=0; j<letrasf2.length; j++) {
+						if(numerolr1[i]!=0) {
+							if(letrasf1[i]==letrasf2[j]&&numerolr1[i]==numerolr2[j]) {
+								repetidas++;
+							}
+						}
+					}
+				}
+				
+				for(int i=0; i<letrasf1.length; i++) {
+					if(numerolr1[i]!=0) {
+						letrastotales++;
+					}
+				}
+				
+				if(repetidas==letrastotales) {
+					System.out.println("Es un anagrama");
+				}else {
+					System.out.println("no es un anagrama");
+				}
 			}else {
 				System.out.println("no es un anagrama");
 			}
+			do {
+				System.out.println("quiere salir del programa");
+				System.out.println("1.si 2.no");
+				salir=scn.nextInt();
+				if(salir!=1&&salir!=2) {
+					System.out.println("no entendi tu respuesta lo puedes volver a escribir");
+				}
+			}while(salir!=1&&salir!=2);
 			
 		}while(salir!=1);
+		
+		System.out.println("hasta otra");
+		
+		scn.close();
+		sct.close();
 		
 	}
 	private static String pedir(Scanner sc) {
@@ -72,6 +111,23 @@ public class semanal6_1 {
 			}
 		}
 	}
-	private static void 
-
+	private static void eliminarrepetidos(char[] letrasf, int[] numerolr) {
+		for(int i=0; i<letrasf.length; i++) {
+			for(int j=0; j<letrasf.length; j++) {
+				if(i!=j) {
+					if(letrasf[i]==letrasf[j]) {
+						for(int y=j; y<letrasf.length; y++) {
+							if(y!=(letrasf.length-1)) {
+								letrasf[y]=letrasf[y+1];
+								numerolr[y]=numerolr[y+1];
+							}else {
+								letrasf[y]=0;
+								numerolr[y]=0;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
